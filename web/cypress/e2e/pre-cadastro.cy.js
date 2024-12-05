8f3a1d9444ed56d01f14d5f90f170ce74fd6ea7b
leonardopadilha <leonardopadilha28@gmail.com>
2024-12-05 05:50:28 -0300

    Ajuste de formatação e realizando teste com contains e parent

--- web/cypress/e2e/pre-cadastro.cy.js
+++ web/cypress/e2e/pre-cadastro.cy.js
@@ -1,54 +1,83 @@
 /// <reference types="cypress" />
 
 describe('Pré-cadastro', () => {
-  it('Deve realizar o pré-cadastro do cliente', () => {
-    cy.visit('/')
+    it('Deve realizar o pré-cadastro do cliente', () => {
+        cy.visit('/')
 
-    cy.get('header nav a[href="pre-cadastro"]')
+        cy.get('header nav a[href="pre-cadastro"]')
+            .click()
+
+        cy.get('form h2')
+            .should('be.visible')
+            .and('have.text', 'Seus dados')
+
+        cy.get('input[name="nome"]')
+            .type('Leonardo Padilha')
+
+        cy.get('input[name="email"]')
+            .type('leonardo@email.com')
+
+        cy.contains('button[type="submit"]', 'Continuar')
+            .click()
+
+        cy.get('.user-name')
+            .should('be.visible')
+            .and('have.text', 'Leonardo Padilha')
+
+        cy.get('.user-email')
+            .should('be.visible')
+            .and('have.text', 'leonardo@email.com')
+    })
+
+    it('Campos obrigatórios', () => {
+        cy.visit('/')
+
+        cy.get('header nav a[href="pre-cadastro"]')
         .click()
 
-    cy.get('form h2')
-        .should('be.visible')
-        .and('have.text', 'Seus dados')
+        cy.get('form h2')
+            .should('be.visible')
+            .and('have.text', 'Seus dados')
 
-    cy.get('input[name="nome"]')
-        .type('Leonardo Padilha')
+        cy.contains('button[type="submit"]', 'Continuar')
+            .click()
 
-    cy.get('input[name="email"]')
-        .type('leonardo@email.com')
+        cy.get('.alert-msg')
+            .should('be.visible')
+            .and('include.text', 'O campo nome é obrigatório.')
 
-    cy.contains('button[type="submit"]', 'Continuar')
-        .click()
+        cy.get('.alert-msg')
+            .should('be.visible')
+            .and('include.text', 'O campo e-mail é obrigatório.')
+        
+    })
 
-    cy.get('.user-name')
-        .should('be.visible')
-        .and('have.text', 'Leonardo Padilha')
-
-    cy.get('.user-email')
-        .should('be.visible')
-        .and('have.text', 'leonardo@email.com')
-  })
-
-  it('Campos obrigatórios', () => {
-    cy.visit('/')
-
-    cy.get('header nav a[href="pre-cadastro"]')
-    .click()
-
-    cy.get('form h2')
-        .should('be.visible')
-        .and('have.text', 'Seus dados')
-
-    cy.contains('button[type="submit"]', 'Continuar')
-        .click()
-
-    cy.get('.alert-msg')
-        .should('be.visible')
-        .and('include.text', 'O campo nome é obrigatório.')
-
-    cy.get('.alert-msg')
-        .should('be.visible')
-        .and('include.text', 'O campo nome é obrigatório.')
+    it('Campos obrigatórios, validação com xpath', () => {
+        cy.visit('/')
     
+        cy.get('header nav a[href="pre-cadastro"]')
+        .click()
+    
+        cy.get('form h2')
+            .should('be.visible')
+            .and('have.text', 'Seus dados')
+    
+        cy.contains('button[type="submit"]', 'Continuar')
+            .click()
+
+        //label[text()="Nome Completo"]/..//div[contains(@class, "alert-msg")]
+
+        cy.contains('label', 'Nome Completo')
+            .parent()
+            .find('.alert-msg')
+            .should('be.visible')
+            .and('have.text', 'O campo nome é obrigatório.')
+
+        cy.contains('label', 'Nome Completo')
+            .parent()
+            .find('.alert-msg')
+            .should('be.visible')
+            .and('have.text', 'O campo e-mail é obrigatório.')
+        
     })
 })
