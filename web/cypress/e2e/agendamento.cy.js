052ca2e4a046cd66efaa90370ded78e26c81be27
leonardopadilha <leonardopadilha28@gmail.com>
2024-12-18 06:21:41 -0300

    Realizando primeiro teste de agendamento

--- web/cypress/e2e/agendamento.cy.js
+++ web/cypress/e2e/agendamento.cy.js
@@ -11,5 +11,35 @@ describe('Agendamento', () => {
 
     // Checkpoint
     cy.contains('span', 'Membros da Equipe').should('be.visible')
+
+    cy.contains('div', 'Tina')
+        .parent()
+        .click()
+
+    // Checkpoint
+    cy.contains('span', 'Serviços').should('be.visible')
+
+    cy.contains('div', 'Combo')
+        .parent()
+        .click()
+
+    // Checkpoint
+    cy.contains('span', 'Dias Disponíveis').should('be.visible')  
+    cy.contains('span', 'Horários Disponíveis').should('be.visible')  
+
+    cy.contains('.dia-semana', '21').click()
+
+    cy.contains('.hora-opcao', '09:00').click()
+
+    cy.contains('button', 'Confirmar e reservar').click()
+
+    // Checkpoint
+    cy.get('h2')
+        .should('be.visible')
+        .and('have.text', 'Let`s Rock')
+
+    cy.get('h3')
+        .should('be.visible')
+        .and('have.text', 'Tudo certo por aqui! Seu horário está confirmado.')
   })
 })
