a3aa8cb70d40429590b1afbbd0b9479c5f8be73d
leonardopadilha <leonardopadilha28@gmail.com>
2025-01-04 10:57:58 -0300

    Iniciando login no modo mobile

--- mobile/cypress/e2e/login.cy.js
+++ mobile/cypress/e2e/login.cy.js
@@ -2,5 +2,41 @@ describe('Login', () => {
   it('Deve logar como barbeiro', () => {
     cy.viewport('iphone-xr')
     cy.visit('/')
+
+    const funcionario = {
+      matricula: 1007,
+      senha: 'pwd123',
+      nome: 'Slash'
+    }
+
+    cy.contains('p', 'Faça login com a sua conta')
+        .should('be.visible')
+
+    cy.login(funcionario)
+    cy.verificaUsuarioLogado(funcionario)
   })
+})
+
+Cypress.Commands.add('login', (funcionario) => {
+  cy.get('input[name="matricula"]')
+      .type(funcionario.matricula)
+
+  cy.get('input[name="senha"]')
+      .type(funcionario.senha)
+
+  cy.contains('button', 'Entrar')
+      .click()
+})
+
+Cypress.Commands.add('verificaUsuarioLogado', (funcionario) => {
+    // trabalhando com escopo do elemento (elementos filhos)
+    cy.get('.usuario-logado').within(() => {
+      cy.get('small')
+          .should('be.visible')
+          .and('have.text', `Olá ${funcionario.nome},`)
+
+      cy.get('h2')
+          .should('be.visible')
+          .and('have.text', 'esse é o seu painel de atendimento!')
+    })
 })
