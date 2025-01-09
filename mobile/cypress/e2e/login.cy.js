dc86043620f306bf4d39b8fbc6be94e43156170a
leonardopadilha <leonardopadilha28@gmail.com>
2025-01-09 07:21:29 -0300

    Adicionando beforeEach

--- mobile/cypress/e2e/login.cy.js
+++ mobile/cypress/e2e/login.cy.js
@@ -1,7 +1,17 @@
+/// <reference types="cypress" />
+
 describe('Login', () => {
-  it('Deve logar como barbeiro', () => {
+  beforeEach(() => {
     cy.viewport('iphone-xr')
     cy.visit('/')
+
+
+    cy.contains('p', 'Faça login com a sua conta')
+        .should('be.visible')
+  });
+
+
+  it('Deve logar como barbeiro', () => {
 
     const funcionario = {
       matricula: 1007,
@@ -9,34 +19,7 @@ describe('Login', () => {
       nome: 'Slash'
     }
 
-    cy.contains('p', 'Faça login com a sua conta')
-        .should('be.visible')
-
     cy.login(funcionario)
     cy.verificaUsuarioLogado(funcionario)
   })
-})
-
-Cypress.Commands.add('login', (funcionario) => {
-  cy.get('input[name="matricula"]')
-      .type(funcionario.matricula)
-
-  cy.get('input[name="senha"]')
-      .type(funcionario.senha)
-
-  cy.contains('button', 'Entrar')
-      .click()
-})
-
-Cypress.Commands.add('verificaUsuarioLogado', (funcionario) => {
-    // trabalhando com escopo do elemento (elementos filhos)
-    cy.get('.usuario-logado').within(() => {
-      cy.get('small')
-          .should('be.visible')
-          .and('have.text', `Olá ${funcionario.nome},`)
-
-      cy.get('h2')
-          .should('be.visible')
-          .and('have.text', 'esse é o seu painel de atendimento!')
-    })
 })
