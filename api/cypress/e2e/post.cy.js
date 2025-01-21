563718452f23d9154dffc6e0a0e14cc3605a168c
leonardopadilha <leonardopadilha28@gmail.com>
2025-01-21 06:23:39 -0300

    Erro ao cadastrar um agendamento que já existe

--- api/cypress/e2e/post.cy.js
+++ api/cypress/e2e/post.cy.js
@@ -3,12 +3,6 @@
 describe('POST /api/agendamentos', () => {
 
   it('Deve criar um novo agendamento', () => {
-
-    cy.deleteMany(
-      { matricula: '1001' },
-      { collection: 'agendamentos' }
-    )
-
     const body = {
       "emailCliente": "testeApi@email.com",
       "nomeCliente": "Teste Api",
@@ -18,17 +12,55 @@ describe('POST /api/agendamentos', () => {
       "codigoServico": "1"
     }
 
-    cy.api({
+    cy.deleteMany(
+      { matricula: body.matricula },
+      { collection: 'agendamentos' }
+    )
+
+    cy.postAgendamento(body)
+      .should((response) => {
+        expect(response.status).to.eq(201)
+        expect(response.body.message).to.eq("Agendamento criado com sucesso")
+        expect(response.body.agendamentoId).to.match(/^[a-f0-9]{24}$/)
+      })
+  })
+
+  it('Deve retornar erro quando o agendamento já existe', () => {
+    const body = {
+      "emailCliente": "leonardo@email.com",
+      "nomeCliente": "Leo Padilha",
+      "data": "20/12/2024",
+      "hora": "10:00",
+      "matricula": "1002",
+      "codigoServico": "2"
+    }
+
+    cy.deleteMany(
+      { matricula: body.matricula },
+      { collection: 'agendamentos' }
+    )
+
+    cy.postAgendamento(body)
+      .should((response) => {
+        expect(response.status).to.eq(201)
+      })
+
+    cy.postAgendamento(body)
+      .should((response) => {
+        expect(response.status).to.eq(409)
+        expect(response.body.message).to.eq("Já existe um agendamento para esta data e hora. Por favor, escolha outro horário.")
+      })
+  })
+})
+
+  Cypress.Commands.add('postAgendamento', (body) => {
+    return cy.api({
       method: 'POST',
       url: '/api/agendamentos',
-      headers: {
-        'Authorization': 'Bearer 3a8a9b8fae87baf503e7c5fe5b97fd72'
-      },
-      body: body
-    }).should((response) => {
-      expect(response.status).to.eq(201)
-      expect(response.body.message).to.eq("Agendamento criado com sucesso")
-      expect(response.body.agendamentoId).to.match(/^[a-f0-9]{24}$/)
-    })
+    headers: {
+      'Authorization': 'Bearer 3a8a9b8fae87baf503e7c5fe5b97fd72'
+    },
+    body: body,
+    failOnStatusCode: false
   })
 })
